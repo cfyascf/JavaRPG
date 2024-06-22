f59e3f0d9beac29721c4e0903db88f5df49cd2de
JuanoneD <juandiego.jd42@gmail.com>
2024-06-21 23:35:12 -0300

    Aparentemente tudo certo

--- Dragon.java
+++ Dragon.java
@@ -2,11 +2,11 @@ public class Dragon extends Monstros{
 
     Dragon(){
         this.descricao ="Dragão são figuras lendárias, representadas por grandes répteis monstruosos que possuem a capacidade de voar e soltar fogo pela boca.";
-        this.danoAtaque = 5;
+        this.danoAtaque = 7;
         this.vidaMonstro = 20;
         this.armaduraMostro = 5;
         this.nivelMonstro = 1;
-        this.modificadorAtaque = 5;
+        this.modificadorAtaque = 8;
     }
 
     @Override
